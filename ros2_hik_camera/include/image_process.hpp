60c62eee837177577f4be0af610f20b08655bafa
LitheshSari <1513503320@qq.com>
2024-04-28 04:08:36 -0700

    [Format]: seperate the image_process.hpp

--- ros2_hik_camera/include/image_process.hpp
+++ ros2_hik_camera/include/image_process.hpp
@@ -28,9 +28,9 @@
 #define SIDE_LEN_B 45.5f
 
 // For model inference
-#define MODEL_INPUT_SHAPE       cv::Size(640, 640)
-#define MODEL_SCORE_THRESHOLD   0.45f
-#define MODEL_NMS_THRESHOLD     0.50f
+#define MODEL_INPUT_SHAPE cv::Size(640, 640)
+#define MODEL_SCORE_THRESHOLD 0.45f
+#define MODEL_NMS_THRESHOLD 0.50f
 
 namespace ExchangeInfo
 {
@@ -110,113 +110,22 @@ namespace image_process
     class ImageProcessNode : public rclcpp::Node
     {
     public:
-        explicit ImageProcessNode(const rclcpp::NodeOptions &options) : Node("image_process", options)
-        {
-            bool use_sensor_data_qos = this->declare_parameter("use_sensor_data_qos", false);
-            auto qos = use_sensor_data_qos ? rmw_qos_profile_sensor_data : rmw_qos_profile_default;
-
-            is_serial_used_ = this->declare_parameter("is_serial_used", false);
-            if (is_serial_used_)
-            {
-                transport_serial_.setPort("/dev/ttyUSB0");
-                transport_serial_.setBaudrate(115200);
-                serial::Timeout _time = serial::Timeout::simpleTimeout(2000);
-                transport_serial_.setTimeout(_time);
-                transport_serial_.setStopbits(serial::stopbits_one);
-
-                transport_serial_.open();
-            }
-
-            if (is_serial_used_)
-            {
-                if (!transport_serial_.isOpen())
-                {
-                    RCLCPP_ERROR(this->get_logger(), "Serial Port is not open!");
-                    return;
-                }
-                else
-                    RCLCPP_INFO(this->get_logger(), "Seiral Port is open.");
-            }
-            else
-                RCLCPP_INFO(this->get_logger(), "Not using Serial Port.");
-
-            // may throw ament_index_cpp::PackageNotFoundError exception
-            std::string package_share_directory = ament_index_cpp::get_package_share_directory("hik_camera");
-            const std::string model_path = package_share_directory + "/model/s_pose_4boxes_12points.onnx";
-
-            is_gpu_ = this->declare_parameter("is_gpu", true);
-            img_processor_.model_.init(model_path, is_gpu_, MODEL_INPUT_SHAPE, MODEL_SCORE_THRESHOLD, MODEL_NMS_THRESHOLD);
-
-            image_sub_ = image_transport::create_camera_subscription(this, "image_raw",
-                                                                     std::bind(&ImageProcessNode::imageCallback, this, std::placeholders::_1, std::placeholders::_2),
-                                                                     "raw", qos);
-
-            processed_image_pub_ = image_transport::create_publisher(this, "processed_image", qos);
-
-            process_thread_ = std::thread{[this]() -> void
-                                          {
-                                              uint8_t send_data_buffer[17];
-
-                                              while (rclcpp::ok())
-                                              {
-                                                  if (frame_queue_.empty())
-                                                      continue;
-                                                  std::lock_guard<std::mutex> lock(g_mutex);
-                                                  // only use the newest frame
-                                                  cv_bridge::CvImageConstPtr frame = std::move(frame_queue_.back());
-                                                  // Warning : do not unlock mutex earlier, because the speed of in-queue is faster
-                                                  // largely than out-queue operation.
-                                                  // RCLCPP_INFO(this->get_logger(), "%d!", frame_queue_.size());
-
-                                                  img_processor_.ModelPredict(frame->image);
-                                                  img_processor_.AddPredictResult(frame->image, true, false);
-                                                  if (img_processor_.SolvePnP(frame->image))
-                                                  {
-                                                      AddSolvePnPResult(frame->image);
-                                                      img_processor_.AddImagePoints(frame->image);
-                                                      if (is_serial_used_)
-                                                      {
-                                                          // READY FOR TEST
-                                                          // sendSerialData(img_processor_.getTvec(), img_processor_.getRvec());
-                                                          uint8_t pack_offset = 3;
-                                                          send_data_buffer[0] = 0x23;
-                                                          send_data_buffer[1] = MsgStream::PC2BOARD | MsgType::AUTO_EXCHANGE;
-                                                          send_data_buffer[2] = 12;
-                                                          for (int i = 0; i < 3; ++i)
-                                                          {
-                                                              uint16_t send_temp = img_processor_.getTvec()[i] * 10.f;
-                                                              send_data_buffer[2 * i + pack_offset] = send_temp;
-                                                              send_data_buffer[2 * i + 1 + pack_offset] = send_temp >> 8;
-                                                              send_temp = img_processor_.getRvec()[i] * 10000.f;
-                                                              send_data_buffer[2 * i + 6 + pack_offset] = send_temp;
-                                                              send_data_buffer[2 * i + 1 + 6 + pack_offset] = send_temp >> 8;
-                                                          }
-                                                          uint16_t crc_result = CRC16_Calc(send_data_buffer, 15);
-                                                          send_data_buffer[15] = crc_result;
-                                                          send_data_buffer[16] = crc_result >> 8;
-                                                          transport_serial_.write(send_data_buffer, sizeof(send_data_buffer));
-                                                      }
-                                                  }
-
-                                                  processed_image_pub_.publish(frame->toImageMsg());
-                                                  std::queue<cv_bridge::CvImageConstPtr> emptyQueue;
-                                                  std::swap(frame_queue_, emptyQueue);
-                                              }
-                                          }};
-
-            RCLCPP_INFO(this->get_logger(), "Starting Image Process!");
-        }
-
-        ~ImageProcessNode()
-        {
-            image_sub_.shutdown();
-            processed_image_pub_.shutdown();
-            if (process_thread_.joinable())
-                process_thread_.join();
-            RCLCPP_INFO(this->get_logger(), "ImageProcessNode destroyed!");
-        }
+        explicit ImageProcessNode(const rclcpp::NodeOptions &options);
+        ~ImageProcessNode();
 
     private:
+        /// @brief the Callback function called by the subscriptor
+        void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg, const sensor_msgs::msg::CameraInfo::ConstSharedPtr &camera_info);
+        /// @brief send the 6-D information data through serial
+        void sendSerialData(const cv::Vec3f &tvec, const cv::Vec3f &rvec);
+        /// @brief for testing SolvePnP(); draw xyz coordinate system
+        void AddSolvePnPResult(cv::Mat img);
+
+    private:
+        bool is_gpu_;
+        bool is_serial_used_;
+        bool is_debug_;
+
         image_transport::CameraSubscriber image_sub_;
         image_transport::Publisher processed_image_pub_;
 
@@ -227,77 +136,7 @@ namespace image_process
         std::mutex g_mutex;
 
         ImageProcessor img_processor_;
-        bool is_gpu_;
-        bool is_serial_used_;
         serial::Serial transport_serial_;
-
-        /// @brief the Callback function called by the subscriptor
-        void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg, const sensor_msgs::msg::CameraInfo::ConstSharedPtr &camera_info)
-        {
-            std::lock_guard<std::mutex> lock(g_mutex);
-            if (!img_processor_.hasCalibrationCoefficient)
-            {
-                img_processor_.GetCameraInfo(camera_info);
-                img_processor_.hasCalibrationCoefficient = true;
-                img_processor_.initFPStick();
-            }
-            try
-            {
-                frame_queue_.push(cv_bridge::toCvShare(msg, "bgr8"));
-            }
-            catch (const cv_bridge::Exception &e)
-            {
-                auto logger = rclcpp::get_logger("ImageProcess Error");
-                RCLCPP_ERROR(logger, "Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-            }
-        }
-
-        void sendSerialData(const cv::Vec3f &tvec, const cv::Vec3f &rvec)
-        {
-            uint8_t pack_offset = 3;
-            uint8_t send_data_buffer[17];
-            send_data_buffer[0] = 0x23;
-            send_data_buffer[1] = MsgStream::PC2BOARD | MsgType::AUTO_EXCHANGE;
-            send_data_buffer[2] = 12;
-            for (int i = 0; i < 3; ++i)
-            {
-                uint16_t send_temp = tvec[i] * 10.f;
-                send_data_buffer[2 * i + pack_offset] = send_temp;
-                send_data_buffer[2 * i + 1 + pack_offset] = send_temp >> 8;
-                send_temp = rvec[i] * 10000.f;
-                send_data_buffer[2 * i + 6 + pack_offset] = send_temp;
-                send_data_buffer[2 * i + 1 + 6 + pack_offset] = send_temp >> 8;
-            }
-            uint16_t crc_result = CRC16_Calc(send_data_buffer, 15);
-            send_data_buffer[15] = crc_result;
-            send_data_buffer[16] = crc_result >> 8;
-            transport_serial_.write(send_data_buffer, sizeof(send_data_buffer));
-        }
-
-        /// @brief for testing SolvePnP(); draw xyz coordinate system
-        void AddSolvePnPResult(cv::Mat img)
-        {
-            cv::Point3f real_center{};
-            std::vector<cv::Point3f> xyz_points;
-            for (unsigned int i = 0; i < 3; ++i)
-            {
-                cv::Vec3f tmp{};
-                tmp(i) = LEN_A;
-                xyz_points.emplace_back(std::move(tmp));
-            }
-
-            cv::Point2i real_center_pic = img_processor_.Object2Picture(real_center);
-            for (auto &point : xyz_points)
-            {
-                cv::Point2i point_pic = img_processor_.Object2Picture(point);
-                cv::arrowedLine(img, real_center_pic, point_pic, cv::Scalar(255, 0, 255), 2);
-            }
-        }
     };
 
-    const std::vector<cv::Point3f> ImageProcessor::object_points_{
-        {LEN_A, LEN_B, 0.0f}, {LEN_A, LEN_A, 0.0f}, {LEN_B, LEN_A, 0.0f}, {-LEN_B, LEN_A, 0.0f}, {-LEN_A, LEN_A, 0.0f}, {-LEN_A, LEN_B, 0.0f}, {-LEN_A, -LEN_B, 0.0f}, {-LEN_A, -LEN_A, 0.0f}, {-LEN_B, -LEN_A, 0.0f}, {LEN_B, -LEN_A, 0.0f}, {LEN_A, -LEN_A, 0.0f}, {LEN_A, -LEN_B, 0.0f}};
-    // the right plate
-    const std::vector<cv::Point3f> ImageProcessor::side_plate_object_points_{
-        {144.0f, SIDE_LEN_A, -SIDE_LEN_A - SIDE_LEN_B}, {144.0f, 0, SIDE_LEN_B}, {144.0f, -SIDE_LEN_A, -SIDE_LEN_A - SIDE_LEN_B}};
 } // namespace image_process
